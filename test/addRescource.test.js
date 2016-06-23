331cf101a72a3122d3d97da5ba811fa91608b7cb
PeterEB <peter.eb9@gmail.com>
2016-06-23 19:07:30 +0800

    ongoing

---
+++ test/addRescource.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect, 
+    smartobject = require('../index.js'); 
+
+describe('AddRescource Check', function () {
+
+
+
+});
---
+++ test/has.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect, 
+    smartobject = require('../index.js'); 
+
+describe('Has Check', function () {
+
+
+
+});
