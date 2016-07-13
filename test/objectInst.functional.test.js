c3887442ed90f951ff3c9bff13a0efcaa72a9ecc
PeterEB <peter.eb9@gmail.com>
2016-07-13 17:24:12 +0800

    test done

--- test/objectInst.functional.test.js
+++ test/objectInst.functional.test.js
@@ -1,7 +1,10 @@
-var expect = require('chai').expect,
+var util = require('util'),
+    _ = require('busyman'),
+    expect = require('chai').expect,
     ObjectInstance = require('../lib/object_instance'); 
 
-var objectInst = new ObjectInstance('temperature', 0);
+var objectInst = new ObjectInstance('temperature', 0),
+    objectInstPwr = new ObjectInstance('pwrMea', 0);
 
 describe('Object Instance - Functional Check', function () {
     describe('#.init()', function () {
@@ -62,39 +65,291 @@ describe('Object Instance - Functional Check', function () {
         });
     });
 
-    describe('#.dump()', function () {
-        it('should', function () {
-            // objectInst.dump();
+    describe('#.dumpSync()', function () {
+        it('should return object instance', function () {
+            var obj = {
+                sensorValue: {
+                    read: '_read_'
+                },
+                units: 'f'
+            };
+
+            expect(objectInst.dumpSync()).to.be.eql(obj);
         });
     });
 
-    describe('#.dumpSync()', function () {
-        it('should', function () {
-            // objectInst.dumpSync();
+    describe('#.dump()', function () {
+        it('should get whole object instance', function (done) {
+            var obj = { 
+                sensorValue: 20,
+                units: 'c',
+                5702: '_unreadable_',
+                5703: 'x',
+                5704: '_exec_'
+            };
+
+            objectInst.init({ 
+                5700: 20,
+                5701: {
+                    read: function (cb) {
+                        cb(null, 'c');
+                    }    
+                },
+                5702: {
+                    write: function (val, cb) {
+                        cb(null, val);
+                    }    
+                },
+                5703: {
+                    read: function (cb) {
+                        cb(null, 'x');
+                    },
+                    write: function (val, cb) {
+                        cb(null, val);
+                    }    
+                },
+                5704: {
+                    exec: function (val, cb) {
+                        if (val === 20)
+                            cb(null, true);
+                        else 
+                            cb(null, false);
+                    } 
+                }
+            });
+
+            objectInst.dump(function (err, result) {
+                if (_.isEqual(result, obj)) {
+                    done();
+                }
+            });
+        });
+
+        it('should get whole object instance whit restrict is true', function (done) {
+            var obj = { 
+                instActivePwr: 20,
+                minMeaActivePwr: 'c',
+                activePwrCal: '_unreadable_',
+                currCal: 'x',
+                resetCumulEnergy: '_exec_'
+            };
+
+            objectInstPwr.init({ 
+                5800: 20,
+                5801: 'c',
+                5806: 'd',
+                5821: 'x',
+                5822: {
+                    exec: function (val, cb) {
+                        if (val === 20)
+                            cb(null, true);
+                        else 
+                            cb(null, false);
+                    } 
+                }
+            });
+
+            objectInstPwr.dump(function (err, result) {
+                if (_.isEqual(result, obj)) {
+                    done();
+                }
+            }, { restrict: true });
         });
     });
 
     describe('#.read()', function () {
-        it('should', function () {
-            // objectInst.read();
+        it('should pass the read value when Resources is a primitive', function (done) {
+            objectInst.read(5700, function (err, result) {
+                if (result === 20) 
+                    done();
+            });
+        });
+
+        it('should pass the read value when Resources is readable', function (done) {
+            objectInst.read(5701, function (err, result) {
+                if (result === 'c') 
+                    done();
+            });
+        });
+
+        it('should pass the read value when Resources is readable and writable', function (done) {
+            objectInst.read(5703, function (err, result) {
+                if (result === 'x') 
+                    done();
+            });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            objectInstPwr.read(5800, function (err, result) {
+                if (result === 20) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            objectInstPwr.read(5801, function (err, result) {
+                if (result === 'c') 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            objectInstPwr.read(5821, function (err, result) {
+                if (result === 'x') 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unreadable_ when restrict is true', function (done) {
+            objectInstPwr.read(5806, function (err, result) {
+                if (err && result === '_unreadable_') 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unreadable_ when Resources is writable', function (done) {
+            objectInst.read(5702, function (err, result) {
+                if (result === '_unreadable_')
+                    done();
+            });
+        });
+
+        it('should pass _exec_ when Resources is excutable ', function (done) {
+            objectInst.read(5704, function (err, result) {
+                if (result === '_exec_')
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            objectInst.read(5706, function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
         });
     });
 
     describe('#.write()', function () {
-        it('should', function () {
-            // objectInst.write();
+        it('should write Resource and pass the write value when Resources is a primitive', function (done) {
+            objectInst.write(5700, 22, function (err, result) {
+                if ((result === 22) && (objectInst.get(5700) === 22))
+                    done();
+            });
+        });
+
+        it('should and pass the write value when Resources is writable', function (done) {
+            objectInst.write(5702, 24, function (err, result) {
+                if (result === 24)
+                    done();
+            });
+        });
+
+        it('should pass the write value when Resources is readable and writable', function (done) {
+            objectInst.write(5703, 26, function (err, result) {
+                if (result === 26)
+                    done();
+            });
+        });
+
+        it('should pass the write value when restrict is true', function (done) {
+            objectInstPwr.write(5806, 'f', function (err, result) {
+                if (result === 'f' && (objectInstPwr.get(5806) === 'f')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the write value when restrict is true', function (done) {
+            objectInstPwr.write(5821, 'y', function (err, result) {
+                if (result === 'y' && (objectInstPwr.get(5821) === 'y')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the write value when restrict is true', function (done) {
+            objectInstPwr.write(5800, 30, function (err, result) {
+                if (err && result === '_unwritable_' && (objectInstPwr.get(5800) === 20)) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unwritable_ when restrict is true', function (done) {
+            objectInstPwr.write(5801, 'x', function (err, result) {
+                if (err && result === '_unwritable_' && (objectInstPwr.get(5801) === 'c')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unwritable_ when Resources is readable', function (done) {
+            objectInst.write(5701, 20, function (err, result) {
+                if (result === '_unwritable_')
+                    done();
+            });
+        });
+
+        it('should pass _exec_ when Resources is excutable', function (done) {
+            objectInst.write(5704, 20, function (err, result) {
+                if (result === '_exec_')
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            objectInst.write(5706, 'C', function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
         });
     });
 
     describe('#.exec()', function () {
-        it('should', function () {
-            // objectInst.exec();
+        it('should exec Resource and pass true through its second argument', function (done) {
+            objectInst.exec(5704, [ 20 ], function (err, result) {
+                if (result === true)
+                    done();
+            });
+        });
+
+        it('should pass _unexecutable_ when Resources is a primitive', function (done) {
+            objectInst.exec(5700, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass _unexecutable_ when Resources is readable', function (done) {
+            objectInst.exec(5701, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass _unexecutable_ when Resources is writable', function (done) {
+            objectInst.exec(5702, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass not argus error', function (done) {
+            objectInst.exec(5704, '20', function (err, result) {
+                if (err)
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            objectInst.exec(5706, [ 20 ], function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
         });
     });
 
     describe('#.clear()', function () {
-        it('should', function () {
-            // objectInst.clear();
+        it('should return object instance and remove all Resources', function () {
+            expect(objectInst.clear()).to.be.eql(new ObjectInstance('temperature', 0));
+            expect(objectInst.get('sensorValue')).to.be.eql(undefined);
+            expect(objectInst.get('units')).to.be.eql(undefined);
         });
     });
 });
--- test/smartObject.functional.test.js
+++ test/smartObject.functional.test.js
@@ -1,4 +1,5 @@
-var expect = require('chai').expect, 
+var _ = require('busyman'),
+    expect = require('chai').expect, 
     SmartObject = require('../index.js'); 
 
 var smartObj = new SmartObject();
@@ -45,42 +46,424 @@ describe('Smart Object - Functional Check', function () {
     });
 
     describe('#.has()', function () {
+        it('should return true if target is exist', function () {
+            expect(smartObj.has(3303, 0, 5700)).to.be.true;
+            expect(smartObj.has('3303', '0', '5700')).to.be.true;
+            expect(smartObj.has('temperature', 0, 'sensorValue')).to.be.true;
+        });
 
+        it('should return false if target is not exist', function () {
+            expect(smartObj.has(3303, 0, 5702)).to.be.false;
+            expect(smartObj.has(3303, 0, 5703)).to.be.false;
+            expect(smartObj.has(3304, 0, 5700)).to.be.false;
+            expect(smartObj.has(3304, 0, 5701)).to.be.false;
+        });
     });
 
     describe('#.findObject()', function () {
+        it('should return object if target is exist', function () {
+            expect(smartObj.findObject(3303)).to.be.eql(smartObj.temperature);
+            expect(smartObj.findObject('3303')).to.be.eql(smartObj.temperature);
+            expect(smartObj.findObject('temperature')).to.be.eql(smartObj.temperature);
+        });
 
+        it('should return undefined if target is not exist', function () {
+            expect(smartObj.findObject(3304)).to.be.eql(undefined);
+            expect(smartObj.findObject(3305)).to.be.eql(undefined);
+            expect(smartObj.findObject('3304')).to.be.eql(undefined);
+            expect(smartObj.findObject('temper')).to.be.eql(undefined);
+        });
     });
 
-    describe('#.findsmartObjance()', function () {
+    describe('#.findObjectInstance()', function () {
+        it('should return object instance if target is exist', function () {
+            expect(smartObj.findObjectInstance(3303, 0)).to.be.eql(smartObj.temperature[0]);
+            expect(smartObj.findObjectInstance(3303, 1)).to.be.eql(smartObj.temperature[1]);
+            expect(smartObj.findObjectInstance('3303', '0')).to.be.eql(smartObj.temperature[0]);
+            expect(smartObj.findObjectInstance('temperature', 0)).to.be.eql(smartObj.temperature[0]);
+        });
 
+        it('should return undefined if target is not exist', function () {
+            expect(smartObj.findObjectInstance(3304, 0)).to.be.eql(undefined);
+            expect(smartObj.findObjectInstance(3305, 0)).to.be.eql(undefined);
+            expect(smartObj.findObjectInstance('3304', '1')).to.be.eql(undefined);
+            expect(smartObj.findObjectInstance('temper', '2')).to.be.eql(undefined);
+        });
     });
 
     describe('#.get()', function () {
+        it('should return rsc if target is exist', function () {
+            expect(typeof smartObj.get(3303, 0, 5700).read).to.be.eql('function');
+            expect(typeof smartObj.get('3303', '0', '5700').read).to.be.eql('function');
+            expect(typeof smartObj.get('temperature', 0, 'sensorValue').read).to.be.eql('function');
+        });
 
+        it('should return undefined if target is not exist', function () {
+            expect(smartObj.get(3303, 0, 5702)).to.be.eql(undefined);
+            expect(smartObj.get(3303, 0, 5703)).to.be.eql(undefined);
+        });
     });
 
     describe('#.set()', function () {
+        it('should return true and add a Resource', function () {
+            expect(smartObj.set(3303, 1, 5701, 'f')).to.be.true;
+            expect(smartObj.get(3303, 1, 5701)).to.be.eql('f');
+        });
 
+        it('should return false if target is not exist', function () {
+            expect(smartObj.set(3303, 2, 5702, 'xx')).to.be.false;
+            expect(smartObj.set(3304, 0, 5703, 'xx')).to.be.false;
+        });
     });
 
     describe('#.dumpSync()', function () {
+        it('should return whole smartObj', function () {
+            var obj = {
+                temperature: {
+                    0: {
+                        sensorValue: {
+                            read: '_read_'
+                        }
+                    }, 
+                    1: {
+                        units: 'f'
+                    }
+                }
+            };
 
+            expect(smartObj.dumpSync()).to.be.eql(obj);
+        });
+
+        it('should return object', function () {
+            var obj = {
+                0: {
+                    sensorValue: {
+                        read: '_read_'
+                    }
+                }, 
+                1: {
+                    units: 'f'
+                }
+            };
+
+            expect(smartObj.dumpSync(3303)).to.be.eql(obj);
+        });
+
+        it('should return object instance', function () {
+            var obj = {
+                sensorValue: {
+                    read: '_read_'
+                }
+            };
+
+            expect(smartObj.dumpSync(3303, 0)).to.be.eql(obj);
+        });
     });
 
     describe('#.dump()', function () {
+        it('should get whole smartObj', function (done) {
+            var obj = { 
+                temperature: {
+                    0: {
+                        sensorValue: 20,
+                        units: 'c',
+                        5702: '_unreadable_',
+                        5703: 'x',
+                        5704: '_exec_'
+                    },
+                    1: {
+                        units: 'f'
+                    }
+                }
+            };
 
+            smartObj.init(3303, 0, { 
+                sensorValue: 20,
+                units: {
+                    read: function (cb) {
+                        cb(null, 'c');
+                    }    
+                },
+                5702: {
+                    write: function (val, cb) {
+                        cb(null, val);
+                    }    
+                },
+                5703: {
+                    read: function (cb) {
+                        cb(null, 'x');
+                    },
+                    write: function (val, cb) {
+                        cb(null, val);
+                    }    
+                },
+                5704: {
+                    exec: function (val, cb) {
+                        if (val === 20)
+                            cb(null, true);
+                        else 
+                            cb(null, false);
+                    } 
+                }
+            });
+
+            smartObj.dump(function (err, result) {
+                if (_.isEqual(result, obj)) 
+                    done();
+            });
+        });
+
+        it('should get an object', function (done) {
+            var obj = { 
+                0: {
+                    sensorValue: 20,
+                    units: 'c',
+                    5702: '_unreadable_',
+                    5703: 'x',
+                    5704: '_exec_'
+                },
+                1: {
+                    units: 'f'
+                }
+            };
+
+            smartObj.dump(3303, function (err, result) {
+                if (_.isEqual(result, obj)) 
+                    done();
+            });
+        });
+
+        it('should get an object instance', function (done) {
+            var obj = { 
+                sensorValue: 20,
+                units: 'c',
+                5702: '_unreadable_',
+                5703: 'x',
+                5704: '_exec_'
+            };
+
+            smartObj.dump(3303, 0, function (err, result) {
+                if (_.isEqual(result, obj)) 
+                    done();
+            });
+        });
+
+        it('should get an object instance whit restrict is true', function (done) {
+            var obj = { 
+                instActivePwr: 20,
+                minMeaActivePwr: 'c',
+                activePwrCal: '_unreadable_',
+                currCal: 'x',
+                resetCumulEnergy: '_exec_'
+            };
+
+            smartObj.init(3305, 0, { 
+                5800: 20,
+                5801: 'c',
+                5806: 'd',
+                5821: 'x',
+                5822: {
+                    exec: function (val, cb) {
+                        if (val === 20)
+                            cb(null, true);
+                        else 
+                            cb(null, false);
+                    } 
+                }
+            });
+
+            smartObj.dump(3305, 0, function (err, result) {
+                if (_.isEqual(result, obj)) 
+                    done();
+            }, { restrict: true });
+        });
     });
 
     describe('#.read()', function () {
+        it('should pass the read value when Resources is a primitive', function (done) {
+            smartObj.read(3303, 0, 5700, function (err, result) {
+                if (result === 20) 
+                    done();
+            });
+        });
 
+        it('should pass the read value when Resources is readable', function (done) {
+            smartObj.read(3303, 0, 5701, function (err, result) {
+                if (result === 'c') 
+                    done();
+            });
+        });
+
+        it('should pass the read value when Resources is readable and writable', function (done) {
+            smartObj.read(3303, 0, 5703, function (err, result) {
+                if (result === 'x') 
+                    done();
+            });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            smartObj.read(3305, 0, 5800, function (err, result) {
+                if (result === 20) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            smartObj.read(3305, 0, 5801, function (err, result) {
+                if (result === 'c')
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the read value when restrict is true', function (done) {
+            smartObj.read(3305, 0, 5821, function (err, result) {
+                if (result === 'x') 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unreadable_ when restrict is true', function (done) {
+            smartObj.read(3305, 0, 5806, function (err, result) {
+                if (err && result === '_unreadable_') 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unreadable_ when Resources is writable', function (done) {
+            smartObj.read(3303, 0, 5702, function (err, result) {
+                if (err && result === '_unreadable_')
+                    done();
+            });
+        });
+
+        it('should pass _exec_ when Resources is excutable ', function (done) {
+            smartObj.read(3303, 0, 5704, function (err, result) {
+                if (err && result === '_exec_')
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            smartObj.read(3303, 0, 5706, function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
+        });
     });
 
     describe('#.write()', function () {
+        it('should write Resource and pass the write value when Resources is a primitive', function (done) {
+            smartObj.write(3303, 0, 5700, 22, function (err, result) {
+                if ((result === 22) && (smartObj.get(3303, 0, 5700) === 22))
+                    done();
+            });
+        });
 
+        it('should and pass the write value when Resources is writable', function (done) {
+            smartObj.write(3303, 0, 5702, 24, function (err, result) {
+                if (result === 24)
+                    done();
+            });
+        });
+
+        it('should pass the write value when Resources is readable and writable', function (done) {
+            smartObj.write(3303, 0, 5703, 26, function (err, result) {
+                if (result === 26)
+                    done();
+            });
+        });
+
+        it('should pass the write value when restrict is true', function (done) {
+            smartObj.write(3305, 0, 5806, 'f', function (err, result) {
+                if (result === 'f' && (smartObj.get(3305, 0, 5806) === 'f')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass the write value when restrict is true', function (done) {
+            smartObj.write(3305, 0, 5821, 'y', function (err, result) {
+                if (result === 'y' && (smartObj.get(3305, 0, 5821) === 'y')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unwritable_ when restrict is true', function (done) {
+            smartObj.write(3305, 0, 5800, 30, function (err, result) {
+                if (err && result === '_unwritable_' && (smartObj.get(3305, 0, 5800) === 20)) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unwritable_ when restrict is true', function (done) {
+            smartObj.write(3305, 0, 5801, 'x', function (err, result) {
+                if (err && result === '_unwritable_' && (smartObj.get(3305, 0, 5801) === 'c')) 
+                    done();
+            }, { restrict: true });
+        });
+
+        it('should pass _unwritable_ when Resources is readable', function (done) {
+            smartObj.write(3303, 0, 5701, 20, function (err, result) {
+                if (result === '_unwritable_')
+                    done();
+            });
+        });
+
+        it('should pass _exec_ when Resources is excutable', function (done) {
+            smartObj.write(3303, 0, 5704, 20, function (err, result) {
+                if (result === '_exec_')
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            smartObj.write(3303, 0, 5706, 'C', function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
+        });
     });
 
     describe('#.exec()', function () {
+        it('should exec Resource and pass true through its second argument', function (done) {
+            smartObj.exec(3303, 0, 5704, [ 20 ], function (err, result) {
+                if (result === true)
+                    done();
+            });
+        });
 
+        it('should pass _unexecutable_ when Resources is a primitive', function (done) {
+            smartObj.exec(3303, 0, 5700, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass _unexecutable_ when Resources is readable', function (done) {
+            smartObj.exec(3303, 0, 5701, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass _unexecutable_ when Resources is writable', function (done) {
+            smartObj.exec(3303, 0, 5702, [ 20 ], function (err, result) {
+                if (result === '_unexecutable_')
+                    done();
+            });
+        });
+
+        it('should pass not argus error', function (done) {
+            smartObj.exec(3303, 0, 5704, '20', function (err, result) {
+                if (err)
+                    done();
+            });
+        });
+
+        it('should pass not found error', function (done) {
+            smartObj.exec(3303, 0, 5706, [ 20 ], function (err, result) {
+                if (err && result === '_notfound_')
+                    done();
+            });
+        });
     });
 });
